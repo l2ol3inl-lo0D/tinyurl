f4ef0f6c963f5c8398c4d1e5a3bd799f9c096bd2
Robin Caron <caron.robin.it@gmail.com>
2024-10-01 20:03:31 -0400

    Add the crossorigin

--- src/main/java/com/notarius/application/controller/RequestController.java
+++ src/main/java/com/notarius/application/controller/RequestController.java
@@ -6,14 +6,12 @@ import com.notarius.application.service.TinyUrlService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET, RequestMethod.POST})
 public class RequestController {
 
     private final TinyUrlService tinyUrlService;
